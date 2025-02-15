db24060c51a9bf7509c5eda1a35ef4333e167a3a
Axel Ulmestig <axel.ulmestig@gmail.com>
2025-02-15 20:34:04 +0100

    fix scheduling script to not overwrite existing config

--- scripts/schedule-crontab.sh
+++ scripts/schedule-crontab.sh
@@ -5,11 +5,12 @@ set -e
 # run every minute
 CRON_CONFIG="* * * * * cd $PWD && make refresh-route53-ip"
 EXISTING_CONFIG="$(crontab -l)"
+LINE_BREAK=$'\n'
 
 if echo "$EXISTING_CONFIG" | grep -cFxq "$CRON_CONFIG"; then
   echo "crontab already configured"
   exit 0
 fi
 
-echo "$CRON_CONFIG" | crontab -
+echo "$EXISTING_CONFIG$LINE_BREAK$CRON_CONFIG" | crontab -
 echo "crontab updated"
