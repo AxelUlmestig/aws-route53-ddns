98398dda701c36133509a117175bb90d4d6c50de
Axel Ulmestig <axel.ulmestig@gmail.com>
2025-11-10 21:31:17 +0000

    fix the cron scheduling

--- scripts/schedule-crontab.sh
+++ scripts/schedule-crontab.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-# run every minute
-CRON_CONFIG="* * * * * cd $PWD && make refresh-route53-ip"
+# runs every minute
+#
+# Explanation of why this looks so horrible:
+#
+# 1. Because the aws cli is installed using nix flakes and direnv, we need to call "nix-command flakes".
+#
+# 2. cron doesn't get the PATH variable from the normal shell. So we need to provide the full path of nix
+CRON_CONFIG="* * * * * cd /home/pi/aws-route53-ddns && /nix/var/nix/profiles/default/bin/nix --extra-experimental-features \"nix-command flakes\" develop --command make refresh-route53-ip > /home/pi/aws-route53-ddns.log 2>&1"
 EXISTING_CONFIG="$(crontab -l)"
 LINE_BREAK=$'\n'
 
